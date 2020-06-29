1ef4e4d0f3e7df8a3a0c72be3999a14cf452e35e
zhuzhengbin <2382798239@qq.com>
2020-06-29 23:20:57 +0800

    【开发】整合Swagger到项目

--- src/main/java/com/zhuzb/github/controller/HomeController.java
+++ src/main/java/com/zhuzb/github/controller/HomeController.java
@@ -3,6 +3,8 @@ package com.zhuzb.github.controller;
 import com.zhuzb.github.common.ResponseMessage;
 import com.zhuzb.github.domain.Admin;
 import com.zhuzb.github.service.HomeService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,17 +17,20 @@ import java.util.List;
  * @Date: 2020/6/19 23:01
  * @Description: 首页控制器
  */
+@Api(tags = "首页控制器")
 @RestController
 @RequestMapping("/home")
 public class HomeController {
     @Autowired
     private HomeService homeService;
 
+    @ApiOperation("欢迎接口")
     @GetMapping("/welcome")
     public ResponseMessage<String> welcome() {
         return ResponseMessage.success("欢迎访问");
     }
 
+    @ApiOperation("查询所有管理员信息")
     @GetMapping("/queryAll")
     public ResponseMessage<List<Admin>> queryAll() {
         List<Admin> admins = homeService.queryAll();
