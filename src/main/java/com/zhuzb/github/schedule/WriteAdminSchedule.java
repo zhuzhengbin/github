cdec40ab67d8b87406309275eadc9c678729f760
zhuzhengbin <2382798239@qq.com>
2020-07-01 23:34:13 +0800

    【开发】引入sl4j日志系统(log4j2作为具体实现)

--- src/main/java/com/zhuzb/github/schedule/WriteAdminSchedule.java
+++ src/main/java/com/zhuzb/github/schedule/WriteAdminSchedule.java
@@ -4,6 +4,8 @@ import com.zhuzb.github.domain.Admin;
 import com.zhuzb.github.mapper.HomeMapper;
 import com.zhuzb.github.utils.DateUtil;
 import com.zhuzb.github.utils.StringUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -18,6 +20,8 @@ import org.springframework.stereotype.Component;
 @EnableScheduling
 public class WriteAdminSchedule {
 
+    private static final Logger log = LoggerFactory.getLogger(WriteAdminSchedule.class);
+
     @Autowired
     HomeMapper homeMapper;
 
@@ -31,5 +35,6 @@ public class WriteAdminSchedule {
         admin.setUsername(DateUtil.currentTimeStr());
         admin.setPassword(StringUtil.uuid().substring(0, 10));
         homeMapper.save(admin);
+        log.info("写入数据库成功");
     }
 }
